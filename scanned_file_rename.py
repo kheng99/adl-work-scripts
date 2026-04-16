310a3185523cb019dc96d7767eda957915cf3edb
Eng Kai Hang <me@kheng.work>
2026-04-16 17:37:28 +0800

    feature: added multiple files support

--- scanned_file_rename.py
+++ scanned_file_rename.py
@@ -8,17 +8,19 @@ import csv
 
 print("Loading CSV files...")
 
-input_file_name_format = "format"
-filename = 'FILENAME.csv'
-emp_id_row_num = # CHECK
-emp_name_row_num = # CHECK
-filter_words = []
+input_file_name_format = "FORMAT"
+filenames = []
+# The row number of each file must correspond to the order of the filenames
+emp_id_row_num = [] # CHECK
+emp_name_row_num = [] # CHECK
+filter_words = [] # Add each word that you want filtered out of the employee name for data sanitization purpose
 
-inactive = dict()
-with open(filename, 'r') as file:
+employees = dict()
+for filename in filenames:
+    with open(filename, 'r') as file:
     reader = csv.reader(file)
     for row in reader:
-        employee_name = row[emp_name_row_num].split(' ')
+        employee_name = row[emp_name_row_num[filenames.index(filename)]].split(' ')
         employee_id = 0
         # Removing words from the employee name that would directly conflict with Windows file naming rules
         for word in filter_words:
@@ -27,13 +29,16 @@ with open(filename, 'r') as file:
             except ValueError:
                 pass
         try:
-            employee_id = int(row[emp_id_row_num])
+            employee_id = int(row[emp_id_row_num[filenames.index(filename)]])
+            # To deal with multiple files having potential overlaps
+            if employee_id in employees:
+                pass
         except ValueError:
             pass # This implies the employee in question does NOT exit, hence skipping
             
         # Sanitizing the employee name through removing weird UTF-8 characters by encoding in, and
         # decoding back to, ASCII while skipping characters that can't be converted
-        inactive[employee_id] = ' '.join(employee_name).encode('ascii', 'ignore').decode('ascii')
+        employees[employee_id] = ' '.join(employee_name).encode('ascii', 'ignore').decode('ascii')
 
 print("Loading CSV files done!")
 print("PLEASE SET CHROME OR ANY WEB BROWSERS AS THE DEFAULT APP FOR PDF FILES BEFORE CONTINUING.")
@@ -55,16 +60,16 @@ for file in files:
         user_input = input()
         emp_id = int(user_input)
     except ValueError:
-        for k, v in inactive.items():
+        for k, v in employees.items():
             if user_input.lower() in v.lower():
                 emp_id = k
                 break
-
-    if not inactive[emp_id]: # Empty string is falsey value; essentially checking if the associated name is empty
+                
+    if not employees[emp_id]: # Empty string is falsey value; essentially checking if the associated name is empty
         print("EMPLOYEE ID DOES NOT HAVE AN ASSOCIATED NAME, SKIPPING")
         break
-    
-    name = f"{'_'.join(inactive[emp_id].split(' '))}_{emp_id}.pdf"
+        
+    name = f"{'_'.join(employees[emp_id].split(' '))}_{emp_id}.pdf"
     os.rename(file, name)
     print(f"RENAMED {file} TO {name}")
     
